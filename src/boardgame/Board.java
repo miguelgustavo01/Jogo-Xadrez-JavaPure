34fd2f8ec1b2c0cbb610141adabc25364f284890
Miguel Bernardo <miguelgustavo123.159@gmail.com>
2022-03-07 14:42:05 -0300

    Formatando o tabuleiro

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -34,5 +34,13 @@ public class Board {
 		this.columns = columns;
 	}
 	
+	//METODOS
 	
+	public Piece piece(int row, int column) {
+		return pieces[row][column];
+	}
+	
+	public Piece piece(Position position){
+		return pieces[position.getRow()][position.getColumn()];
+	}
 }
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,25 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+	
+	//ATRIBUTOS
+	
+	private Color color;
+	
+	//CONSTRUTOR ARGUMENTOS
+	
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
+
+	//GETTERS AND SETTERS
+	
+	public Color getColor() {
+		return color;
+	}
+
+}
