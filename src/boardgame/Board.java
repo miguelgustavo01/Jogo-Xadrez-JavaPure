09ede724905611103f5e85a8371b929609efd50e
Miguel Bernardo <miguelgustavo123.159@gmail.com>
2022-03-07 15:25:27 -0300

    BoardException e defensiva do programa

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,6 +11,12 @@ public class Board {
 	//CONSTRUTOR ARGUMENTOS
 	
 	public Board(int rows, int columns) {
+		if(rows <1 || columns <1) {
+			throw new BoardException("Erro ao criar tabuleiro, é necessário que haja ao menos 1 linha e 1 coluna");
+		}
+		
+		
+		
 		this.rows = rows;
 		this.columns = columns;
 		pieces = new Piece[rows][columns];
@@ -22,30 +28,96 @@ public class Board {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
-	}
-
-	public void setColumns(int columns) {
-		this.columns = columns;
 	}
 	
 	//METODOS
 	
 	public Piece piece(int row, int column) {
+		
+		if(!positionExists(row, column)) {
+			throw new BoardException("Posição não encontrada no tabuleiro.");
+		}
+		
 		return pieces[row][column];
 	}
 	
 	public Piece piece(Position position){
+		
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não encontrada no tabuleiro.");
+		}
+		
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
 	public void placePiece(Piece piece, Position position) {
+		
+		if(thereIsAPiece(position)) {
+			throw new BoardException("a posição: " +position +" já esta preenchida.");
+		}
+		
 		pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position= position;
 	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >=0 && column < columns;
+	}
+	
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+	
+	public boolean thereIsAPiece (Position position) {
+		
+		if(!positionExists(position)) {
+			throw new BoardException("Posição não encontrada no tabuleiro.");
+		}
+		
+		return piece(position) != null;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
