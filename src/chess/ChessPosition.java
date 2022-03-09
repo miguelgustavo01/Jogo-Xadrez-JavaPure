743035476be8cc6307a15aac2832d1ac6069a55f
Miguel Bernardo <miguelgustavo123.159@gmail.com>
2022-03-09 12:07:06 -0300

    exeçôes e posições do xadrez

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,49 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+	
+	private char column;
+	private int row;
+	
+	//CONSTRUTOR ARGUMENTOS
+	
+	public ChessPosition(char column, int row) {
+		
+		if(column < 'a' || column > 'h'|| row < 1 || row > 8) {
+			throw new ChessException("Erro ao declarar posição do xadrez, valorez validos de a1 até h8");
+		}
+		
+		this.column = column;
+		this.row = row;
+	}
+	
+	//GETTERS AND SETTERS
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	//METODOS
+	
+	 protected Position toPosition() {
+		 return new Position(8 - row, column - 'a');
+	 }
+	
+	 protected static ChessPosition fromPosition(Position position) {
+		 return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+	 }
+	 
+	 //TOSTRING
+	 
+	 @Override
+	 public String toString() {
+		return "" + column + row;
+	 }
+
+}
